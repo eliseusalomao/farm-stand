94ed19771911cb292a2775ee4096f6faa8c08815
eliseusalomao <eliseusalomao20@gmail.com>
2024-06-08 20:38:03 -0300

    fix: selected value of category
    Use a loop to display the options that can beselected on a form.
    
    When editing the pre-selected value is the current one.

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+const categories = ['fruits', 'vegetable', 'dairy']
+
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     res.render('products/index', { products })
 })
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', { categories })
 })
 
 app.get('/products/:id', async (req, res) => {
@@ -42,13 +44,19 @@ app.post('/products', async (req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params
     const product = await Product.findById(id)
-    res.render('products/edit', { product })
+    res.render('products/edit', { product, categories })
 })
 
 app.put('/products/:id', async (req, res) => {
     const { id } = req.params
     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
     res.redirect(`/products/${product._id}`)
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params
+    await Product.findByIdAndDelete(id)
+    res.redirect('/products')
 })
 
 app.listen(3000, () => {
