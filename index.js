c3b2e54994e31e98bb0eeb11a13545d585ddac84
eliseusalomao <eliseusalomao20@gmail.com>
2024-06-08 18:43:49 -0300

    feat: view and show routes
    Add view and show routes with respectvely views pages

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
+const Product = require('./models/Product')
 
-mongoose.connect('mongodb://localhost:27017/shopApp')
+mongoose.connect('mongodb://localhost:27017/farmStand')
     .then(() => {
         console.log('Connection open.')
     })
@@ -13,6 +14,17 @@ mongoose.connect('mongodb://localhost:27017/shopApp')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products })
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    res.render('products/show', { product })
+})
 
 app.listen(3000, () => {
     console.log('App is listening on port 3000')
