9f4afce707460c15a72382e0db7ae6f3b5ab949c
eliseusalomao <eliseusalomao20@gmail.com>
2024-06-08 17:50:45 -0300

    feat: add connection with mongo and fix mongoose dep

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/shopApp')
+    .then(() => {
+        console.log('Connection open.')
+    })
+    .catch(err => {
+        console.log(`An error has occurred:\n${err}`)
+    })
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
