09091199f75ca42af3f16fa3156bc3cd7d6175ae
eliseusalomao <eliseusalomao20@gmail.com>
2024-06-08 20:15:03 -0300

    feat: add edit view and route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
 const Product = require('./models/Product')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://localhost:27017/farmStand')
     .then(() => {
@@ -15,6 +16,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
@@ -35,6 +37,18 @@ app.post('/products', async (req, res) => {
     const product = new Product(req.body)
     await product.save()
     res.redirect(`products/${product._id}`)
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    res.redirect(`/products/${product._id}`)
 })
 
 app.listen(3000, () => {
